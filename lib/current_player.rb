ca8565fab5e8c52b2b0198346ef36a4dfad2d43c
kzavgorodnya <kateryna.zavgorodnya@gmail.com>
2018-07-16 19:46:27 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -6,11 +6,10 @@ def turn_count (board)
   return counter
 end
 
-#
-# def current_player (board)
-#   if counter % 2 == 0
-#     return "X"
-#   else
-#     return "O"
-# end
-# end
+def current_player (board)
+  if turn_count(board)% 2 == 0
+    return "X"
+  else
+    return "O"
+end
+end
